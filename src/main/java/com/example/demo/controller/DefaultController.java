585e4ead9f7323c6b855f9245ccf2de1ef3b21e8
Vytautas A. <vytautasasm@gmail.com>
2024-12-01 10:25:54 -0600

    Updates.

---
+++ src/main/java/com/example/demo/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DefaultController {
+
+    @GetMapping
+    public String home() {
+        return "Hello world!<br>This is a homepage for a Web Forum App JSON-based API.<br>You can find the documentation at <a href=\"https://github.com/vytaux/zylyty-web-forum-app\">https://github.com/vytaux/zylyty-web-forum-app</a>.";
+    }
+}
